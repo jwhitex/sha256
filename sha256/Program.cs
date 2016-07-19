841ea4970fc79e2abc47cf681671f5e7b38f8228
jwhites <mobycharles@gmail.com>
2016-07-18 22:48:03 -0400

    Add project files.

---
+++ sha256/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sha256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
